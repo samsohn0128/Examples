a331a85b86b312fffd10092c3fc0a07aea48dea7
손동우 <samsohn0128@gmail.com>
2023-01-05 17:47:10 +0900

    SOAPLE react - mini-blog

--- react/mini-blog/src/App.js
+++ react/mini-blog/src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+import MainPage from "./component/page/MainPage";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import styled from "styled-components";
+import PostWritePage from "./component/page/PostWritePage";
+import PostViewPage from "./component/page/PostViewPage";
+
+const MainTitleText = styled.p`
+  font-size: 24px;
+  font-weight: bold;
+  text-align: center;
+`
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+        <MainTitleText>붕붕바라바라붕붕</MainTitleText>
+        <Routes>
+            <Route index element={<MainPage />}/>
+            <Route path="post-write" element={<PostWritePage />} />
+            <Route path="posts/:postId" element={<PostViewPage />} />
+        </Routes>
+    </BrowserRouter>
   );
 }
 
